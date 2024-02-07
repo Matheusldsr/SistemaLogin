67ff069ef6388010ebad26e5c6004c90f259d747
Matheusldsr <140101892+Matheusldsr@users.noreply.github.com>
2024-02-07 13:15:48 -0300

    Delete Cadastro_Corretores directory

--- Cadastro_Corretores/bancodedados.sql
+++
@@ -1,7 +0,0 @@
-Create database sistemalogin;
-CREATE TABLE corretores (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    cpf VARCHAR(11) UNIQUE NOT NULL,
-    creci VARCHAR(8) NOT NULL
-);
